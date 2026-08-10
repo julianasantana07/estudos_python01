77fee84445dacb36e3e71c337ce64c9c0a29f42a
Juliana <julianasantana3342@gmail.com>
2026-08-09 21:32:42 -0300

    Adiciona calculadora em Python

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+print("-=" * 20)
+print("TABUADA".center(35))
+print("-=" * 20)
+
+num1 = int(input("Digite um número e descubra a tabuada dele: "))
+for i in range(1,11):
+    
+    print(f"{num1} X {i:2} = {num1 * i:3}")
+
+print("-=" * 20)
+print("FIM".center(35))
+print("-=" * 20)
+
+
