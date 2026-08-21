9e21b4fed6ac86cfc511925b6aa117526a4f059b
Juliana <julianasantana3342@gmail.com>
2026-08-20 21:56:46 -0300

    Tabuada

---
+++ tabuada.py
@@ -0,0 +1,14 @@
+print("-=" * 20)
+print("DESCUBRA A TABUADA DO NUMERO...".center(40))
+print("-=" * 20)
+num = int(input("Digite um número: "))
+
+for i in range(1,11):
+    print(f"{num} x {i} = {num * i}")
+
+print("-=" * 20)
+print("FIM DA TABUADA!".center(40))
+print("-=" * 20)
+
+
+
